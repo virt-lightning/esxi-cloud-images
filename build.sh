2d5fbebd00eb1788d5c939979cfa774ce7f03b29
Gonéri Le Bouder <goneri@lebouder.net>
2019-05-01 11:08:45 -0400

    ensure we can access the final image in libvirt datastore

--- build.sh
+++ build.sh
@@ -89,6 +89,6 @@ virt-install --connect qemu:///system \
 	--accelerate --network=network:default,model=e1000 \
 	--hvm --graphics vnc,listen=0.0.0.0
 
-
+sudo chmod 644 /var/lib/libvirt/images/esxi-${VERSION}.qcow2
 qemu-img convert -f qcow2 -O qcow2 -c /var/lib/libvirt/images/esxi-${VERSION}.qcow2 esxi-${VERSION}.qcow2
 sudo virsh undefine --remove-all-storage esxi-${VERSION}
